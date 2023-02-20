80fe3c122b19aefe67c3afd98eb66644f24dcd6d
1imo <timohoyland@icloud.com>
2023-02-20 15:37:54 +0000

    Public Infrastructure And Onboarding System Redesigned

--- next-app/app/search/page.jsx
+++ next-app/app/search/page.jsx
@@ -1,5 +1,19 @@
+"use client"
+
 export default function Search() {
+
+    const getLocation = () => {
+        const successCallback = (position) => {
+            console.log(position);
+          };
+          
+          const errorCallback = (error) => {
+            console.log(error);
+          };
+          
+          navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    }
     return <div>
-        Search
+        <button onClick={() => getLocation()}>Location</button>
     </div>
 }
